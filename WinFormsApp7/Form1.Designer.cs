9690c1b2f6e13fe3fa6fa0d17172db0467603498
PelancongAngkasa <yusufathagunawan@gmail.com>
2024-07-24 10:36:14 +0700

    Sanitasi NIK + fitur tabel

--- WinFormsApp7/Form1.Designer.cs
+++ WinFormsApp7/Form1.Designer.cs
@@ -44,28 +44,28 @@
             tableLayoutPanel1.Controls.Add(titleLabel, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
-            tableLayoutPanel1.Margin = new Padding(4);
+            tableLayoutPanel1.Margin = new Padding(6, 7, 6, 7);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 7;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 34F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 34F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 34F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 34F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 34F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 34F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 57F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 57F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 57F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 57F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 57F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 57F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
-            tableLayoutPanel1.Size = new Size(424, 281);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
+            tableLayoutPanel1.Size = new Size(606, 468);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // textBox1
             // 
             textBox1.Dock = DockStyle.Fill;
-            textBox1.Location = new Point(4, 72);
-            textBox1.Margin = new Padding(4);
+            textBox1.Location = new Point(6, 121);
+            textBox1.Margin = new Padding(6, 7, 6, 7);
             textBox1.MaxLength = 4;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(98, 23);
+            textBox1.Size = new Size(139, 31);
             textBox1.TabIndex = 0;
             textBox1.TextAlign = HorizontalAlignment.Center;
             textBox1.TextChanged += textBox_TextChanged;
@@ -74,11 +74,11 @@
             // textBox2
             // 
             textBox2.Dock = DockStyle.Fill;
-            textBox2.Location = new Point(110, 72);
-            textBox2.Margin = new Padding(4);
+            textBox2.Location = new Point(157, 121);
+            textBox2.Margin = new Padding(6, 7, 6, 7);
             textBox2.MaxLength = 4;
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(98, 23);
+            textBox2.Size = new Size(139, 31);
             textBox2.TabIndex = 1;
             textBox2.TextAlign = HorizontalAlignment.Center;
             textBox2.TextChanged += textBox_TextChanged;
@@ -87,11 +87,11 @@
             // textBox3
             // 
             textBox3.Dock = DockStyle.Fill;
-            textBox3.Location = new Point(216, 72);
-            textBox3.Margin = new Padding(4);
+            textBox3.Location = new Point(308, 121);
+            textBox3.Margin = new Padding(6, 7, 6, 7);
             textBox3.MaxLength = 4;
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(98, 23);
+            textBox3.Size = new Size(139, 31);
             textBox3.TabIndex = 2;
             textBox3.TextAlign = HorizontalAlignment.Center;
             textBox3.TextChanged += textBox_TextChanged;
@@ -100,11 +100,11 @@
             // textBox4
             // 
             textBox4.Dock = DockStyle.Fill;
-            textBox4.Location = new Point(322, 72);
-            textBox4.Margin = new Padding(4);
+            textBox4.Location = new Point(459, 121);
+            textBox4.Margin = new Padding(6, 7, 6, 7);
             textBox4.MaxLength = 4;
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(98, 23);
+            textBox4.Size = new Size(141, 31);
             textBox4.TabIndex = 3;
             textBox4.TextAlign = HorizontalAlignment.Center;
             textBox4.TextChanged += textBox_TextChanged;
@@ -115,10 +115,10 @@
             labelKK.AutoSize = true;
             tableLayoutPanel1.SetColumnSpan(labelKK, 4);
             labelKK.Dock = DockStyle.Fill;
-            labelKK.Location = new Point(4, 34);
-            labelKK.Margin = new Padding(4, 0, 4, 0);
+            labelKK.Location = new Point(6, 57);
+            labelKK.Margin = new Padding(6, 0, 6, 0);
             labelKK.Name = "labelKK";
-            labelKK.Size = new Size(416, 34);
+            labelKK.Size = new Size(594, 57);
             labelKK.TabIndex = 10;
             labelKK.Text = "Masukkan Nomor Kartu Keluarga:";
             labelKK.TextAlign = ContentAlignment.MiddleCenter;
@@ -127,10 +127,10 @@
             // 
             tableLayoutPanel1.SetColumnSpan(searchButton, 2);
             searchButton.Dock = DockStyle.Fill;
-            searchButton.Location = new Point(110, 106);
-            searchButton.Margin = new Padding(4);
+            searchButton.Location = new Point(157, 178);
+            searchButton.Margin = new Padding(6, 7, 6, 7);
             searchButton.Name = "searchButton";
-            searchButton.Size = new Size(204, 26);
+            searchButton.Size = new Size(290, 43);
             searchButton.TabIndex = 8;
             searchButton.Text = "Cari";
             searchButton.UseVisualStyleBackColor = true;
@@ -140,10 +140,10 @@
             // 
             tableLayoutPanel1.SetColumnSpan(addKKButton, 2);
             addKKButton.Dock = DockStyle.Fill;
-            addKKButton.Location = new Point(110, 174);
-            addKKButton.Margin = new Padding(4);
+            addKKButton.Location = new Point(157, 292);
+            addKKButton.Margin = new Padding(6, 7, 6, 7);
             addKKButton.Name = "addKKButton";
-            addKKButton.Size = new Size(204, 26);
+            addKKButton.Size = new Size(290, 43);
             addKKButton.TabIndex = 9;
             addKKButton.Text = "Tambah KK";
             addKKButton.UseVisualStyleBackColor = true;
@@ -155,21 +155,21 @@
             tableLayoutPanel1.SetColumnSpan(titleLabel, 4);
             titleLabel.Dock = DockStyle.Fill;
             titleLabel.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            titleLabel.Location = new Point(4, 0);
-            titleLabel.Margin = new Padding(4, 0, 4, 0);
+            titleLabel.Location = new Point(6, 0);
+            titleLabel.Margin = new Padding(6, 0, 6, 0);
             titleLabel.Name = "titleLabel";
-            titleLabel.Size = new Size(416, 34);
+            titleLabel.Size = new Size(594, 57);
             titleLabel.TabIndex = 12;
             titleLabel.Text = "Manajemen Kartu Keluarga";
             titleLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(424, 281);
+            ClientSize = new Size(606, 468);
             Controls.Add(tableLayoutPanel1);
-            Margin = new Padding(4);
+            Margin = new Padding(6, 7, 6, 7);
             Name = "Form1";
             Text = "Form1";
             tableLayoutPanel1.ResumeLayout(false);
